716ef53c1b58e4c5cc0c8483b940d90d16d2e5e2
jannes <jannes.hp@t-online.de>
2018-10-12 15:14:16 +0200

    create link for songgroup

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Songgroup } from '../../models/songgroup';
 import { DataService } from '../../services/data.service';
 import { DATABASES } from '../../models/databases';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event',
@@ -15,32 +16,37 @@ export class EventComponent implements OnInit {
   @Output() deleted: EventEmitter<any> = new EventEmitter();
   JSON = JSON;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
 
   songs: string[] = [];
 
-  ngOnInit() { 
-    if(!this.event.songs){
+  ngOnInit() {
+    if (!this.event.songs) {
       this.event = new Songgroup();
     }
     this.setSongs();
   }
 
-  emitEditMeta(songgroup){
+  emitEditMeta(songgroup) {
     this.editMeta.emit(songgroup);
   }
 
-  delete(id){
+  delete(id) {
     this.dataService.delete(DATABASES.events, id);
     this.deleted.emit();
   }
 
-  setSongs(){
+  setSongs() {
     this.event.songs.forEach( uuid => {
       this.dataService.getByKey(DATABASES.songs, uuid).then(res => {
-        if(res)
+        if (res) {
           this.songs.push(res.title);
+        }
       });
     });
+  }
+
+  performSonggroup() {
+    this.router.navigateByUrl('perform/' + this.event.songs.join('_'));
   }
 }
