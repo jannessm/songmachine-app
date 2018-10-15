881489e064a9bae3534a8a8b0e1278053be116c6
jannes <jannes.hp@t-online.de>
2018-10-15 12:22:01 +0200

    fix icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     },
     {
       route: 'browser/events',
-      icon: 'icon-songgroup',
+      icon: 'icon-event',
     },
     {
       route: 'editor',
