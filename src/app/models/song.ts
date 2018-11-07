608e3f45c252162a887d6702215e6701fa943b47
jannes <jannes.hp@t-online.de>
2018-11-07 16:52:38 +0100

    add keyFinder service

--- src/app/models/song.ts
+++ src/app/models/song.ts
@@ -6,6 +6,8 @@ export class Song {
   artist?: string;
   bpm?: number;
   books?: string[];
+  key?: string;
+  transposedBy = 0;
   blocks: Block[] = [];
   order?: string[];
   annotationCells = 0;
