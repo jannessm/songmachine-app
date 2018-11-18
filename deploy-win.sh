ec145cc3f8527ae3876302fe64fd082135daafdc
jannes <jannes.hp@t-online.de>
2018-11-18 22:25:26 +0100

    fix deploy scrits

--- deploy-win.sh
+++ deploy-win.sh
@@ -5,5 +5,5 @@ cp -R dist/ ../deploy-win/
 cd ../deploy-win
 yarn
 cd ../songsheet-app
-electron-packager ../deploy-win Songmachine --overwrite --asar=true --platform=win32 --arch=ia32 --icon=dist/assets/main-icon/win/songsheet.ico --prune=true --out=release-builds --package-manager=yarn --version-string.FileDescription=CE --version-string.ProductName="Songmachine"
+electron-packager ../deploy-win Songmachine --overwrite --platform=win32 --arch=ia32 --icon=dist/assets/main-icon/win/songsheet.ico --prune=true --out=release-builds --package-manager=yarn --version-string.FileDescription=CE --version-string.ProductName="Songmachine"
 rm -rf ../deploy-win
