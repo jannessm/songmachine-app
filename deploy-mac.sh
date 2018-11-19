c1d53ce5ca0b81cbf9f237664628c4564b5ee2d7
jannes <jannes.hp@t-online.de>
2018-11-19 22:41:42 +0100

    fix deployment

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -5,5 +5,5 @@ cp -R dist/ ../deploy-mac/
 cd ../deploy-mac
 yarn
 cd ../songsheet-app
-electron-packager ../deploy-mac --overwrite --platform=darwin --arch=x64 --icon=dist/assets/main-icon/mac/songsheet.icns --prune=true --out=release-builds --package-manager=yarn
+electron-packager ../deploy-mac Songmachine --overwrite --platform=darwin --arch=x64 --icon=dist/assets/main-icon/mac/songsheet.icns --prune=true --out=release-builds --package-manager=yarn
 rm -rf ../deploy-mac
