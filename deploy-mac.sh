239d75537ba68d6922b7427556f182c7a1fe4172
jannes <jannes.hp@t-online.de>
2018-11-19 23:22:56 +0100

    add dmg deployment

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -7,3 +7,6 @@ yarn
 cd ../songsheet-app
 electron-packager ../deploy-mac Songmachine --overwrite --platform=darwin --arch=x64 --icon=dist/assets/main-icon/mac/songsheet.icns --prune=true --out=release-builds --package-manager=yarn
 rm -rf ../deploy-mac
+
+# build installer
+electron-installer-dmg ./release-builds/Songmachine-darwin-x64/Songmachine.app Songmachine --out=./release-builds --icon=dist/assets/main-icon/mac/songsheet.icns --overwrite --background=./dev-tools/dmg-installer-bkg.png --title="Songmachine Installer" 
