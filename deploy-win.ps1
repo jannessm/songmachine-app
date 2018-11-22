e67150247e4ea5328618f8609cf36c3adabebad7
jannes <jannes.hp@t-online.de>
2018-11-22 18:09:25 +0100

    fix deployment for windows

--- deploy-win.ps1
+++ deploy-win.ps1
@@ -1,9 +1,11 @@
-# /bin/bash
-yarn build
+ng build --prod --aot
+Copy-Item src/electron/* -Destination dist/ -Recurse 
+Copy-Item package.json -Destination dist/
+Copy-Item yarn.lock -Destination dist/
 mkdir -p ../deploy-win
-cp -R dist/ ../deploy-win/
+Copy-Item dist/** -Destination ../deploy-win/ -Recurse
 cd ../deploy-win
 yarn
 cd ../songmachine-app
 electron-packager ../deploy-win Songmachine --overwrite --platform=win32 --arch=ia32 --icon=dist/assets/main-icon/win/songsheet.ico --prune=true --out=release-builds --package-manager=yarn --version-string.FileDescription=CE --version-string.ProductName="Songmachine"
-rm -rf ../deploy-win
+rm ../deploy-win -Recurse
