806518640960bc85eaff57c27f5d66f0b6532fae
jannes <jannes.hp@t-online.de>
2018-11-27 12:44:07 +0100

    fix deployment script windows

--- deploy-win.ps1
+++ deploy-win.ps1
@@ -1,11 +1,11 @@
-ng build --prod --aot
-Copy-Item src/electron/* -Destination dist/ -Recurse 
-Copy-Item package.json -Destination dist/
-Copy-Item yarn.lock -Destination dist/
+.\node_modules\.bin\ng build --prod --aot
+Copy-Item src/electron/* -Destination dist/ -Recurse -Force
+Copy-Item package.json -Destination dist/ -Force
+Copy-Item yarn.lock -Destination dist/ -Force
 mkdir -p ../deploy-win
 Copy-Item dist/** -Destination ../deploy-win/ -Recurse
 cd ../deploy-win
 yarn
 cd ../songmachine-app
-electron-packager ../deploy-win Songmachine --overwrite --platform=win32 --arch=ia32 --icon=dist/assets/main-icon/win/songsheet.ico --prune=true --out=release-builds --package-manager=yarn --version-string.FileDescription=CE --version-string.ProductName="Songmachine"
-rm ../deploy-win -Recurse
+.\node_modules\.bin\electron-packager ../deploy-win Songmachine --overwrite --platform=win32 --arch=ia32 --icon=dist/assets/main-icon/win/songsheet.ico --prune=true --out=release-builds --package-manager=yarn --version-string.FileDescription=CE --version-string.ProductName="Songmachine"
+rm ../deploy-win -Recurse -Force
