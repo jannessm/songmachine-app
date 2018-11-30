2b1ca250b6e1aafcbadf2eb671281e84a83a1ee2
jannes <jannes.hp@t-online.de>
2018-11-30 16:36:55 +0100

    add graphs

--- src/app/models/grammars/grammarEditor.ts
+++ src/app/models/grammars/grammarEditor.ts
@@ -66,30 +66,5 @@ export const GrammarEditor: Grammar = {
       ignoreNext: 0
     }
   ],
-  // transitions: {
-  //   'S' : ['S', 'SR', 'SG', 'SB', 'SI', 'SBo', 'SBoI', 'Br', 'S'],
-
-  //   'SR' : ['SR', 'S', 'SG', 'SB', 'SRI', 'SRBo', 'SRBoI', 'BrR', 'SR'],
-  //   'SG' : ['SG', 'SR', 'S', 'SB', 'SGI', 'SGBo', 'SGBoI', 'BrG', 'SG'],
-  //   'SB' : ['SB', 'SR', 'SG', 'S', 'SBI', 'SBBo', 'SBBoI', 'BrB', 'SB'],
-  //   'SBo': ['SBo', 'SRBo', 'SGBo', 'SBBo', 'SBoI', 'S', 'SI', 'BrBo', 'SBo'],
-  //   'SI' : ['SI', 'SRI', 'SGI', 'SBI', 'S', 'SBoI', 'SBo', 'BrI', 'SI'],
-
-  //   'SRI' : ['SRI', 'SI', 'SGI', 'SB', 'SR', 'SRBoI', 'SRBo', 'BrRI', 'SRI'],
-  //   'SGI' : ['SGI', 'SRI', 'SI', 'SB', 'SG', 'SGBoI', 'SGBo', 'BrGI', 'SGI'],
-  //   'SBI' : ['SBI', 'SRI', 'SGI', 'SB', 'SB', 'SBBoI', 'SBBo', 'BrBI', 'SBI'],
-  //   'SBoI' : ['SBoI', 'SRBoI', 'SGBoI', 'SBBoI', 'SBo', 'SI', 'S', 'BrBoI', 'SBoI'],
-
-  //   'SRBo' : ['SRBo', 'SBo', 'SGBo', 'SBBo', 'SRBoI', 'SR', 'SRI', 'BrRBo', 'SRBo'],
-  //   'SGBo' : ['SGBo', 'SRBo', 'SBo', 'SBBo', 'SGBoI', 'SG', 'SGI', 'BrGBo', 'SGBo'],
-  //   'SBBo' : ['SBBo', 'SRBo', 'SGBo', 'SBo', 'SBBoI', 'SB', 'SBI', 'BrBBo', 'SBBo'],
-
-  //   'SRBoI' : ['SRBoI', 'SBoI', 'SGBoI', 'SBBoI', 'SRBo', 'SRI', 'SR', 'BrRBoI', 'SRBoI'],
-  //   'SGBoI' : ['SGBoI', 'SRBoI', 'SBoI', 'SBBoI', 'SGBo', 'SGI', 'SG', 'BrGBoI', 'SGBoI'],
-  //   'SBBoI' : ['SBBoI', 'SRBoI', 'SGBoI', 'SBoI', 'SBBo', 'SBI', 'SB', 'BrBBoI', 'SBBoI'],
-
-  //   'Br' : ['BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'S'],
-  //   'BrCo' : ['BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'BrCo', 'S']
-  // },
 
 };
