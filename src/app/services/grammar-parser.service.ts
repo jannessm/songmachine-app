205f6dd435be7ceaf4235aa964e0a5c2c2ed75e0
jannes <jannes.hp@t-online.de>
2019-02-07 12:56:30 +0100

    remove brackets check because grammar handles it

--- src/app/services/grammar-parser.service.ts
+++ src/app/services/grammar-parser.service.ts
@@ -36,10 +36,6 @@ export class GrammarParser {
     if (!input) {
       return input;
     }
-    const isBalanced = this.checkBalancedBrackets(input);
-    if (!!isBalanced) {
-      return isBalanced;
-    }
 
     let parser;
     if (!keepChars) {
@@ -51,7 +47,7 @@ export class GrammarParser {
     try {
       parser.feed(input);
     } catch (err) {
-      console.log(err);
+      console.log(input, err);
     }
     if (!parser.results || (parser.results && parser.results.length === 0)) {
       return GrammarParser.escapeHTML(input);
@@ -110,37 +106,5 @@ export class GrammarParser {
       }
       return reduced;
     }, []);
-  }
-
-  private checkBalancedBrackets(input: string, tag: string = 'pre'): string {
-    const stack = [];
-    let i = 0;
-    let lastOpened;
-
-    for (i = 0; input && i < input.length; i++) {
-      const char = input[i];
-      if (char === '[' && stack.length === 0) {
-        stack.push(i);
-        lastOpened = i;
-      } else if (char === '[' && stack.length > 0) {
-        return this.composeError(input, stack.pop(), 1, tag);
-      } else if (char === ']' && stack.length === 1) {
-        stack.pop();
-      } else if (char === ']' && stack.length === 0) {
-        return this.composeError(input, i, 1, tag);
-      }
-    }
-
-    if (stack.length === 0) {
-      return '';
-    } else {
-      return this.composeError(input, lastOpened, 1, tag);
-    }
-  }
-
-  private composeError(input: string, from: number, length: number, tag: string = 'pre'): string {
-    return `<${tag}>${input.substr(0, from)}</${tag}>` +
-      `<${tag} class="error">${input.substr(from, length)}</${tag}>` +
-      `<${tag}>${input.substr(from + length)}</${tag}>`;
   }
 }
