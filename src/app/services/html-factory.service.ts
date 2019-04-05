50a893d379eba68d5999df969366af0d591fb34d
jannes <jannes.hp@t-online.de>
2019-04-05 13:50:59 +0200

    fix error of fonts

--- src/app/services/html-factory.service.ts
+++ src/app/services/html-factory.service.ts
@@ -118,70 +118,6 @@ export class HtmlFactoryService {
   }
 
   public style(): string {
-    const fontfamily = `/* latin-ext */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: italic;
-        font-weight: 400;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_ext_RegularItalic.woff2') format('woff2');
-        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-      }
-      /* latin */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: italic;
-        font-weight: 400;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_RegularItalic.woff2') format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* latin-ext */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: italic;
-        font-weight: 600;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_ext_BoldItalic.woff2') format('woff2');
-        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-      }
-      /* latin */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: italic;
-        font-weight: 600;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_BoldItalic.woff2') format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* latin-ext */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: normal;
-        font-weight: 400;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_ext_Regular.woff2') format('woff2');
-        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-      }
-      /* latin */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: normal;
-        font-weight: 400;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_Regular.woff2') format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      /* latin-ext */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: normal;
-        font-weight: 600;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_ext_Bold.woff2') format('woff2');
-        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-      }
-      /* latin */
-      @font-face {
-        font-family: 'Ubuntu Mono';
-        font-style: normal;
-        font-weight: 600;
-        src: url('file://{{__dirname}}/UbuntuMono_latin_Bold.woff2') format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }`;
     return `<style>
       .page {
         width: 100%;
@@ -274,7 +210,6 @@ export class HtmlFactoryService {
         font-weight: normal;
         color: black;
       }
-      ${fontfamily}
       </style>`;
   }
 
