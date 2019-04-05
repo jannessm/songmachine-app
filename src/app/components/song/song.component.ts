b57dceb5883a97996d3feee23c296d82622ff46c
jannes <jannes.hp@t-online.de>
2019-04-06 00:21:12 +0200

    add tooltip on ellipsis

--- src/app/components/song/song.component.ts
+++ src/app/components/song/song.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, OnInit, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 import { Song } from '../../models/song';
 import { DataService } from '../../services/data.service';
@@ -14,10 +14,13 @@ import { StoreService } from '../../services/store.service';
   templateUrl: './song.component.html',
   styleUrls: ['./song.component.scss']
 })
-export class SongComponent {
+export class SongComponent implements AfterViewChecked {
 
   @Input() song: Song;
   @Output() editMeta: EventEmitter<any> = new EventEmitter();
+
+  @ViewChild('title', {read: ElementRef})title: ElementRef;
+  tooltip = false;
 
   constructor(
     private dataService: DataService,
@@ -27,6 +30,14 @@ export class SongComponent {
     private translationService: TranslationService,
     private storeService: StoreService
   ) { }
+
+  ngAfterViewChecked() {
+    if (this.title.nativeElement.offsetWidth < this.title.nativeElement.scrollWidth) {
+      this.tooltip = true;
+    } else {
+      this.tooltip = false;
+    }
+  }
 
   editSong(song: Song) {
     this.router.navigateByUrl('/editor/' + song.id);
